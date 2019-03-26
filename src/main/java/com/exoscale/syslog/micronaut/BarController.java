3b75f82a0ce8d9979cfaddc9fb338f08d226472e
Nicolas Frankel <nicolas.frankel@exoscale.com>
2019-03-26 14:36:31 +0100

    Change endpoint return value to String

--- src/main/java/com/exoscale/syslog/micronaut/BarController.java
+++ src/main/java/com/exoscale/syslog/micronaut/BarController.java
@@ -1,14 +1,10 @@
 package com.exoscale.syslog.micronaut;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BarController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(BarController.class);
 
     private final Bar bar;
 
@@ -17,7 +13,7 @@ public class BarController {
     }
 
     @GetMapping("/")
-    public void root() {
-        LOGGER.info(bar.bar());
+    public String root() {
+        return bar.bar();
     }
 }
