65e28b592462e2b982b99c7b1ef8b3736baf53dc
Nicolas Frankel <nicolas.frankel@exoscale.com>
2019-03-26 14:01:14 +0100

    Add Netty as an embedded web server

--- src/main/java/com/exoscale/syslog/micronaut/Main.java
+++ src/main/java/com/exoscale/syslog/micronaut/Main.java
@@ -1,19 +1,10 @@
 package com.exoscale.syslog.micronaut;
 
-import io.micronaut.context.ApplicationContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.micronaut.runtime.Micronaut;
 
 public class Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
-        ApplicationContext context = ApplicationContext
-                .build()
-                .deduceEnvironment(false)
-                .start();
-        Bar bar = context.getBean(Bar.class);
-        LOGGER.info(bar.bar());
+        Micronaut.run(Main.class);
     }
 }
