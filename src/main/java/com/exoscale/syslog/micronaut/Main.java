9675254bd879ef22c8c9c56fe40633c97c8dbda8
Nicolas Frankel <nicolas.frankel@exoscale.com>
2019-03-27 14:19:20 +0100

    Workaround waiting for 1.1.RC2

--- src/main/java/com/exoscale/syslog/micronaut/Main.java
+++ src/main/java/com/exoscale/syslog/micronaut/Main.java
@@ -1,9 +1,12 @@
 package com.exoscale.syslog.micronaut;
 
+import io.micronaut.core.annotation.TypeHint;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.runtime.Micronaut;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@TypeHint(Get.class)
 public class Main {
 
     public static void main(String[] args) {
